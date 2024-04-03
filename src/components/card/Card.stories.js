2d3b86bb8a2bb7d06e8578f34d2c73dcf22a352d
dnkemper <daniellew@wustl.edu>
2024-04-03 13:31:09 -0500

    updated chromatic

--- src/components/card/Card.stories.js
+++ src/components/card/Card.stories.js
@@ -1,8 +1,8 @@
-import UidsCard from './Card.vue'
-import UidsGrid from '../grid/Grid.vue'
-import UidsGridItem from '../grid/GridItem.vue'
+import ResearchCard from './Card.vue'
+import ResearchGrid from '../grid/Grid.vue'
+import ResearchGridItem from '../grid/GridItem.vue'
 import card_image from '../../assets/images/viewbook/sections/122.jpg'
-import person_image from '../../assets/images/viewbook/sections/herky2.jpeg'
+import person_image from '../../assets/images/viewbook/sections/person-one.svg'
 import Background from '../shared/background'
 import Borderless from '../shared/borderless'
 import Media from '../shared/media'
@@ -17,7 +17,7 @@ export default {
     },
   },
   title: 'Components/Card',
-  component: UidsCard,
+  component: ResearchCard,
   tags: ['autodocs'],
   argTypes: {
     // Props
@@ -211,7 +211,7 @@ export default {
 // More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template = (args) => ({
   // Components used in your story `template` are defined in the `components` object
-  components: { UidsCard },
+  components: { ResearchCard },
   // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     return { args }
@@ -221,7 +221,7 @@ const Template = (args) => ({
     <div :class="args.section_background" style="padding-top: 2rem; padding-bottom: 2rem;">
       <div class="grid--threecol--33-34-33">
         <div class="list-container">
-          <uids-card
+          <research-card
             :url="args.url"
             :link_text="args.link_text"
             :link_indicator="args.link_indicator"
@@ -241,7 +241,7 @@ const Template = (args) => ({
             <template #subtitle v-if="args.subtitle"><div v-html="args.subtitle" ></div></template>
             <template #meta v-if="args.meta"><div v-html="args.meta" ></div></template>
             <template #default><div v-html="args.default"></div></template>
-          </uids-card>
+          </research-card>
         </div>
       </div>
     </div>
@@ -256,8 +256,8 @@ Default.args = {
   subtitle: '',
   meta: '',
   default:
-    'For decades, Iowa City has been a gathering place for artists, creating a cultural hub that’s more accessible than any major city. ',
-  url: 'https://uiowa.edu',
+    'For decades, Washu has been a gathering place for artists, creating a cultural hub that’s more accessible than any major city. ',
+  url: 'https://artsci.wustl.edu',
   link_text: 'Explore the arts',
   link_indicator: true,
   button_align_bottom: false,
@@ -292,8 +292,8 @@ LinkedImage.args = {
   link_text: '',
 }
 
-export const ImageUsingUiowaIcon = Template.bind({});
-ImageUsingUiowaIcon.args = {
+export const ImageUsingResearchIcon = Template.bind({});
+ImageUsingResearchIcon.args = {
   ...Default.args,
   centered: true,
   media_padded: true,
@@ -302,22 +302,22 @@ ImageUsingUiowaIcon.args = {
   media_border: true,
   title: '24hour-phone',
   media:
-    '<img icon="24hour-phone" variant="two-color" src="https://icons.brand.uiowa.edu/brand-icons/megaphone-bullhorn-two-color.png" alt="24hour-phone" loading="lazy" class="two-color">',
+  '<img icon="24hour-phone" variant="two-color" src="https://icons.brand.uiowa.edu/brand-icons/megaphone-bullhorn-two-color.png" alt="24hour-phone" loading="lazy" class="two-color">',
   url: 'https://uiowa.edu',
   default: '',
 }
 
-// export const ImageUsingFontAwesomeIcon = Template.bind({});
-// ImageUsingFontAwesomeIcon.args = {
-//   ...Default.args,
-//   centered: true,
-//   media_size: 'small',
-//   media_padded: true,
-//   title: 'very cool card',
-//   media:
-//     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M430.3 503.8L382.3 447.8C378.4 443.4 376.3 437.7 376.3 431.7V376.3L351.1 344.7V407.8C351.1 425.4 337.7 439.8 319.1 439.8C302.3 439.8 287.1 425.4 287.1 407.8V344.7L263.7 376.3V431.7C263.7 437.7 261.6 443.4 257.7 447.8L209.7 503.8C201.1 513.8 186.1 514.8 176.3 505.9C166.5 497 165.6 481.6 174.3 471.6L216.3 422.5V367.8C216.3 362.3 218.1 357 221.5 352.7L287.1 266.3V266L154.6 387.8C97.58 447.6 .0003 405.2 0 320.6C0 272.7 34.02 232.3 79.35 226.4L232.3 202.5L191.5 161.6C183.7 153.8 182.1 141.5 187.6 131.8L211.5 90.06L173.3 39.18C165.3 28.54 167.2 13.26 177.5 5.046C187.9-3.17 202.7-1.207 210.7 9.429L258.7 73.34C264.6 81.21 265.3 91.99 260.4 100.6L237.8 140L287.1 190.3V152.1C287.1 137.2 298.2 124.7 311.1 121.1V63.93C311.1 59.51 315.6 55.93 319.1 55.93C324.4 55.93 327.1 59.51 327.1 63.93V121.1C341.8 124.7 351.1 137.2 351.1 152.1V190.3L402.2 140L379.6 100.6C374.7 91.99 375.4 81.21 381.3 73.34L429.3 9.429C437.3-1.207 452.1-3.169 462.5 5.047C472.8 13.26 474.7 28.55 466.7 39.18L428.5 90.06L452.4 131.8C457.9 141.5 456.3 153.8 448.5 161.6L407.7 202.5L560.6 226.4C605.1 232.3 640 272.7 640 320.6C640 405.2 542.4 447.6 485.4 387.8L351.1 266V266.3L418.5 352.7C421.9 357 423.7 362.3 423.7 367.8V422.5L465.7 471.6C474.4 481.6 473.5 497 463.7 505.9C453.9 514.8 438.9 513.8 430.3 503.8L430.3 503.8z"/></svg>',
-//   url: 'https://uiowa.edu',
-// }
+export const ImageUsingFontAwesomeIcon = Template.bind({});
+ImageUsingFontAwesomeIcon.args = {
+  ...Default.args,
+  centered: true,
+  media_size: 'small',
+  media_padded: true,
+  title: 'very cool card',
+  media:
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M430.3 503.8L382.3 447.8C378.4 443.4 376.3 437.7 376.3 431.7V376.3L351.1 344.7V407.8C351.1 425.4 337.7 439.8 319.1 439.8C302.3 439.8 287.1 425.4 287.1 407.8V344.7L263.7 376.3V431.7C263.7 437.7 261.6 443.4 257.7 447.8L209.7 503.8C201.1 513.8 186.1 514.8 176.3 505.9C166.5 497 165.6 481.6 174.3 471.6L216.3 422.5V367.8C216.3 362.3 218.1 357 221.5 352.7L287.1 266.3V266L154.6 387.8C97.58 447.6 .0003 405.2 0 320.6C0 272.7 34.02 232.3 79.35 226.4L232.3 202.5L191.5 161.6C183.7 153.8 182.1 141.5 187.6 131.8L211.5 90.06L173.3 39.18C165.3 28.54 167.2 13.26 177.5 5.046C187.9-3.17 202.7-1.207 210.7 9.429L258.7 73.34C264.6 81.21 265.3 91.99 260.4 100.6L237.8 140L287.1 190.3V152.1C287.1 137.2 298.2 124.7 311.1 121.1V63.93C311.1 59.51 315.6 55.93 319.1 55.93C324.4 55.93 327.1 59.51 327.1 63.93V121.1C341.8 124.7 351.1 137.2 351.1 152.1V190.3L402.2 140L379.6 100.6C374.7 91.99 375.4 81.21 381.3 73.34L429.3 9.429C437.3-1.207 452.1-3.169 462.5 5.047C472.8 13.26 474.7 28.55 466.7 39.18L428.5 90.06L452.4 131.8C457.9 141.5 456.3 153.8 448.5 161.6L407.7 202.5L560.6 226.4C605.1 232.3 640 272.7 640 320.6C640 405.2 542.4 447.6 485.4 387.8L351.1 266V266.3L418.5 352.7C421.9 357 423.7 362.3 423.7 367.8V422.5L465.7 471.6C474.4 481.6 473.5 497 463.7 505.9C453.9 514.8 438.9 513.8 430.3 503.8L430.3 503.8z"/></svg>',
+  url: 'https://wustl.edu',
+}
 
 export const WithNoBorder = Template.bind({});
 WithNoBorder.args = {
@@ -328,13 +328,13 @@ WithNoBorder.args = {
 export const PersonProfile = Template.bind({});
 PersonProfile.args = {
   ...Default.args,
-  media: '<img src="' + person_image + '" alt="Herky">',
+  media: '<img src="' + person_image + '" alt="not-found">',
   orientation: 'right',
   media_size: 'small',
   media_shape: 'circle',
   media_border: true,
-  title: 'Herky Hawk',
-  subtitle: 'University of Iowa Mascot',
+  title: 'User not found',
+  subtitle: 'Washington University in St Louis',
   default:
     'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
   borderless: true,
@@ -367,7 +367,7 @@ ButtonAlignedToBottom.args = {
 
 const GridTemplate = (args) => ({
   // Components used in your story `template` are defined in the `components` object
-  components: { UidsGrid, UidsGridItem, UidsCard },
+  components: { ResearchGrid, ResearchGridItem, ResearchCard },
   // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     return { args }
@@ -375,9 +375,9 @@ const GridTemplate = (args) => ({
   // And then the `args` are bound to your component with `v-bind="args"`
   template: `
     <div :class="args.section_background" style="padding-top: 2rem; padding-bottom: 2rem;">
-      <uids-grid :type="args.grid_type">
-        <uids-grid-item v-for="item in args.record_count" :key="item">
-          <uids-card
+      <research-grid :type="args.grid_type">
+        <research-grid-item v-for="item in args.record_count" :key="item">
+          <research-card
             :url="args.url"
             :link_text="args.link_text"
             :link_indicator="args.link_indicator"
@@ -397,9 +397,9 @@ const GridTemplate = (args) => ({
             <template #subtitle v-if="args.subtitle"><div v-html="args.subtitle" ></div></template>
             <template #meta v-if="args.meta"><div v-html="args.meta" ></div></template>
             <template #default><div v-html="args.default"></div></template>
-          </uids-card>
-        </uids-grid-item>
-      </uids-grid>
+          </research-card>
+        </research-grid-item>
+      </research-grid>
     </div>
   `,
 })
